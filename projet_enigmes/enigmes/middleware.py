52bc6cb2aa4735fd94736fe8620a498ead7bbeef
Germain Becker <germain.becker@gmail.com>
2024-11-11 10:17:50 +0100

    correction middleware pour laisser l'accès aux ressources sauf les fichiers md

--- projet_enigmes/enigmes/middleware.py
+++ projet_enigmes/enigmes/middleware.py
@@ -6,6 +6,6 @@ class BlockMarkdownAccessMiddleware:
 
     def __call__(self, request):
         # Bloquer l'accès direct aux fichiers markdown via l'URL
-        if '/markdown/' in request.path or request.path.endswith('.md'):
+        if request.path.endswith('.md'):
             raise Http404("Fichier non trouvé")
         return self.get_response(request)
